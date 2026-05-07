68664410cc11b61d3bec5e8d8e06dac41cfc1f6e
Alisson <alisson_lds@hotmail.com>
2026-05-07 00:24:31 -0300

    Add Data module

--- ProjetoCadastroFuncionariosDass.dpr
+++ ProjetoCadastroFuncionariosDass.dpr
@@ -2,7 +2,8 @@ program ProjetoCadastroFuncionariosDass;
 
 uses
   Vcl.Forms,
-  Un00001FormCadastroFuncionariosDass in 'Un00001FormCadastroFuncionariosDass.pas' {FormCadastroFuncionariosDass};
+  Un00001FormCadastroFuncionariosDass in 'Un00001FormCadastroFuncionariosDass.pas' {FormCadastroFuncionariosDass},
+  Dm00001CadastroFuncionariosDass in 'Dm00001CadastroFuncionariosDass.pas' {DataModule1: TDataModule};
 
 {$R *.res}
 
@@ -10,5 +11,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFormCadastroFuncionariosDass, FormCadastroFuncionariosDass);
+  Application.CreateForm(TDataModule1, DataModule1);
   Application.Run;
 end.
