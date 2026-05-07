4d367ba30a0a0b249ad5fe4494690a6fc6a5237f
Alisson <alisson_lds@hotmail.com>
2026-05-06 23:24:23 -0300

    Primeiro Commit

---
+++ Un00001FormCadastroFuncionariosDass.dfm
@@ -0,0 +1,14 @@
+object FormCadastroFuncionariosDass: TFormCadastroFuncionariosDass
+  Left = 0
+  Top = 0
+  Caption = 'Cadastro de funcion'#225'rios Dass'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+end
---
+++ Un00001FormCadastroFuncionariosDass.pas
@@ -0,0 +1,24 @@
+unit Un00001FormCadastroFuncionariosDass;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFormCadastroFuncionariosDass = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FormCadastroFuncionariosDass: TFormCadastroFuncionariosDass;
+
+implementation
+
+{$R *.dfm}
+
+end.
